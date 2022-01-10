d4d32b7ebd09de3a40048e33a1504e3939239fb1
ponntp <nathapon1161@gmail.com>
2022-01-10 10:24:39 +0700

    red

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -12,7 +12,7 @@
                 <form method="POST" action="/task">
 
                     <div class="form-group">
-                        <textarea name="description" class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"  placeholder='Enter your task'></textarea>  
+                        <textarea name="description" class="bg-red-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"  placeholder='Enter your task'></textarea>  
                         @if ($errors->has('description'))
                             <span class="text-danger">{{ $errors->first('description') }}</span>
                         @endif
