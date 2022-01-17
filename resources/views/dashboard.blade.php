7f7db03cf29a3f4f2f8568a3ac6932097231e8f9
Jamesssoften <60128647+Jamesssoften@users.noreply.github.com>
2022-01-17 09:34:57 +0700

    Update dashboard.blade.php
    แก้ fonts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 style="font-family: 'Courier New', monospace;
+        <h2 style="font-family: 'sans-serif', monospace;
         margin-top: 10px;
         margin-left: 10px;
         font-weight: 600;
@@ -14,7 +14,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
                 <div class="flex">
-                    <div style="font-family: 'Courier New', monospace;
+                    <div style="font-family: 'sans-serif', monospace;
                         margin-top: 5px;
                         margin-left: 10px;
                         margin-bottom:10px;
