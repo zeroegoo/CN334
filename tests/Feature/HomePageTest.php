97cbc01ce3c00e940f5411df74fdb97e8699a0e3
Pasmoa <sahatsawat.td@gmail.com>
2022-03-17 22:57:48 +0700

    aom again

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Fortify\Features;
+use Laravel\Jetstream\Jetstream;
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+
+    public function test_homepage_screen_can_be_rendered()
+    {
+        if (! Features::enabled(Features::registration())) {
+            return $this->markTestSkipped('Registration support is not enabled.');
+        }
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+
+}
